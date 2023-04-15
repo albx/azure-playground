de01acbecd07b6485e885ecffb7ec5b3abd99546
Alberto Mori <albi.mo87@hotmail.it>
2023-04-15 17:49:26 +0200

    messsage become a record

--- azure-queue-storage/AzureQueueStorage.Messages/BookRegisteredMessage.cs
+++ azure-queue-storage/AzureQueueStorage.Messages/BookRegisteredMessage.cs
@@ -1,15 +1,3 @@
 ﻿namespace AzureQueueStorage.Messages;
 
-public class BookRegisteredMessage
-{
-    public BookRegisteredMessage(Guid id, string title, string author)
-    {
-        Id = id;
-        Title = title;
-        Author = author;
-    }
-
-    public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-}
+public record BookRegisteredMessage(Guid Id, string Title, string Author);
